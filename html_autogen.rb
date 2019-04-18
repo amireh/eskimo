9974950687234c2a59edf4963665cffbc1e84242
Ahmad Amireh <ahmad@amireh.net>
2019-04-18 13:03:42 -0600

    Release 3.0.0

---
+++ html_autogen.rb
@@ -0,0 +1,351 @@
+# (Auto-generated) Render an HTML `<a />` tag
+class Eskimo::HTML::A < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<abbr />` tag
+class Eskimo::HTML::Abbr < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<address />` tag
+class Eskimo::HTML::Address < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<area />` tag
+class Eskimo::HTML::Area < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<article />` tag
+class Eskimo::HTML::Article < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<aside />` tag
+class Eskimo::HTML::Aside < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<audio />` tag
+class Eskimo::HTML::Audio < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<b />` tag
+class Eskimo::HTML::B < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<base />` tag
+class Eskimo::HTML::Base < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<bdi />` tag
+class Eskimo::HTML::Bdi < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<bdo />` tag
+class Eskimo::HTML::Bdo < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<blockquote />` tag
+class Eskimo::HTML::Blockquote < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<body />` tag
+class Eskimo::HTML::Body < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<br />` tag
+class Eskimo::HTML::Br < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<button />` tag
+class Eskimo::HTML::Button < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<canvas />` tag
+class Eskimo::HTML::Canvas < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<caption />` tag
+class Eskimo::HTML::Caption < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<cite />` tag
+class Eskimo::HTML::Cite < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<code />` tag
+class Eskimo::HTML::Code < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<col />` tag
+class Eskimo::HTML::Col < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<colgroup />` tag
+class Eskimo::HTML::Colgroup < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<data />` tag
+class Eskimo::HTML::Data < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<datalist />` tag
+class Eskimo::HTML::Datalist < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<dd />` tag
+class Eskimo::HTML::Dd < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<del />` tag
+class Eskimo::HTML::Del < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<details />` tag
+class Eskimo::HTML::Details < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<dfn />` tag
+class Eskimo::HTML::Dfn < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<dialog />` tag
+class Eskimo::HTML::Dialog < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<div />` tag
+class Eskimo::HTML::Div < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<dl />` tag
+class Eskimo::HTML::Dl < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<dt />` tag
+class Eskimo::HTML::Dt < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<em />` tag
+class Eskimo::HTML::Em < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<embed />` tag
+class Eskimo::HTML::Embed < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<fieldset />` tag
+class Eskimo::HTML::Fieldset < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<figcaption />` tag
+class Eskimo::HTML::Figcaption < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<figure />` tag
+class Eskimo::HTML::Figure < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<footer />` tag
+class Eskimo::HTML::Footer < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<form />` tag
+class Eskimo::HTML::Form < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h1 />` tag
+class Eskimo::HTML::H1 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h2 />` tag
+class Eskimo::HTML::H2 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h3 />` tag
+class Eskimo::HTML::H3 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h4 />` tag
+class Eskimo::HTML::H4 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h5 />` tag
+class Eskimo::HTML::H5 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<h6 />` tag
+class Eskimo::HTML::H6 < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<head />` tag
+class Eskimo::HTML::Head < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<header />` tag
+class Eskimo::HTML::Header < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<hgroup />` tag
+class Eskimo::HTML::Hgroup < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<hr />` tag
+class Eskimo::HTML::Hr < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<i />` tag
+class Eskimo::HTML::I < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<iframe />` tag
+class Eskimo::HTML::Iframe < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<img />` tag
+class Eskimo::HTML::Img < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<input />` tag
+class Eskimo::HTML::Input < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<ins />` tag
+class Eskimo::HTML::Ins < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<kbd />` tag
+class Eskimo::HTML::Kbd < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<keygen />` tag
+class Eskimo::HTML::Keygen < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<label />` tag
+class Eskimo::HTML::Label < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<legend />` tag
+class Eskimo::HTML::Legend < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<li />` tag
+class Eskimo::HTML::Li < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<link />` tag
+class Eskimo::HTML::Link < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<main />` tag
+class Eskimo::HTML::Main < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<map />` tag
+class Eskimo::HTML::Map < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<mark />` tag
+class Eskimo::HTML::Mark < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<math />` tag
+class Eskimo::HTML::Math < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<menu />` tag
+class Eskimo::HTML::Menu < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<menuitem />` tag
+class Eskimo::HTML::Menuitem < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<meta />` tag
+class Eskimo::HTML::Meta < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<meter />` tag
+class Eskimo::HTML::Meter < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<nav />` tag
+class Eskimo::HTML::Nav < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<noscript />` tag
+class Eskimo::HTML::Noscript < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<object />` tag
+class Eskimo::HTML::Object < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<ol />` tag
+class Eskimo::HTML::Ol < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<optgroup />` tag
+class Eskimo::HTML::Optgroup < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<option />` tag
+class Eskimo::HTML::Option < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<output />` tag
+class Eskimo::HTML::Output < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<p />` tag
+class Eskimo::HTML::P < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<param />` tag
+class Eskimo::HTML::Param < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<picture />` tag
+class Eskimo::HTML::Picture < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<pre />` tag
+class Eskimo::HTML::Pre < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<progress />` tag
+class Eskimo::HTML::Progress < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<q />` tag
+class Eskimo::HTML::Q < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<rb />` tag
+class Eskimo::HTML::Rb < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<rp />` tag
+class Eskimo::HTML::Rp < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<rt />` tag
+class Eskimo::HTML::Rt < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<rtc />` tag
+class Eskimo::HTML::Rtc < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<ruby />` tag
+class Eskimo::HTML::Ruby < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<s />` tag
+class Eskimo::HTML::S < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<samp />` tag
+class Eskimo::HTML::Samp < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<script />` tag
+class Eskimo::HTML::Script < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<section />` tag
+class Eskimo::HTML::Section < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<select />` tag
+class Eskimo::HTML::Select < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<slot />` tag
+class Eskimo::HTML::Slot < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<small />` tag
+class Eskimo::HTML::Small < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<source />` tag
+class Eskimo::HTML::Source < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<span />` tag
+class Eskimo::HTML::Span < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<strong />` tag
+class Eskimo::HTML::Strong < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<style />` tag
+class Eskimo::HTML::Style < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<sub />` tag
+class Eskimo::HTML::Sub < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<summary />` tag
+class Eskimo::HTML::Summary < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<sup />` tag
+class Eskimo::HTML::Sup < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<svg />` tag
+class Eskimo::HTML::Svg < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<table />` tag
+class Eskimo::HTML::Table < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<tbody />` tag
+class Eskimo::HTML::Tbody < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<td />` tag
+class Eskimo::HTML::Td < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<template />` tag
+class Eskimo::HTML::Template < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<textarea />` tag
+class Eskimo::HTML::Textarea < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<tfoot />` tag
+class Eskimo::HTML::Tfoot < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<th />` tag
+class Eskimo::HTML::Th < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<thead />` tag
+class Eskimo::HTML::Thead < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<time />` tag
+class Eskimo::HTML::Time < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<title />` tag
+class Eskimo::HTML::Title < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<tr />` tag
+class Eskimo::HTML::Tr < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<track />` tag
+class Eskimo::HTML::Track < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<u />` tag
+class Eskimo::HTML::U < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<ul />` tag
+class Eskimo::HTML::Ul < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<var />` tag
+class Eskimo::HTML::Var < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<video />` tag
+class Eskimo::HTML::Video < Eskimo::HTML::Component
+end
+# (Auto-generated) Render an HTML `<wbr />` tag
+class Eskimo::HTML::Wbr < Eskimo::HTML::Component
+end
